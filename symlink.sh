da6e339949aacf6d4cb8d9d1f977357222c2ff57
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-08-21 23:45:08 +0700

    [FEAT] Added symlink script, just run it and it SHOULD work!

---
+++ symlink.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Find the parent directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[@]}" )" &>/dev/null && pwd )
+STOW_DIR="$SCRIPT_DIR"
+
+# The directories to link
+HOME_DIRS=(
+	"${XDG_CONFIG_HOME:-$HOME/.config}"
+)
+
+# Check if directory exists (mkdir if not) AND not a symlink (stow will error if a symlink exists)
+for dir in "${HOME_DIRS[@]}"; do
+	if [ ! -d "$dir" ]; then
+		echo "[MKDIR] $dir"
+		mkdir -p "$dir"
+	elif [ -L "$dir" ]; then
+		echo "[ERROR] $dir: Symlink!" >&2
+		exit 1
+	fi
+done
+
+# Stow mirrors $HOME, so it'll stow the content of $HOME_DIRS instead of the directories themselves
+stow -v "$@" -d "$STOW_DIR" -t "$HOME" "home"
