f26b935a8f2956416c727ac980ac72a2185460fb
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-08-28 18:05:13 +0700

    [FEAT] Forces Hyprland envs variables

--- home/.config/hypr/envs.lua
+++ home/.config/hypr/envs.lua
@@ -6,4 +6,14 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("CLUTTER_BACKEND", "wayland")
+
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
 hl.env("PATH", os.getenv("PATH") .. ":" .. os.getenv("HOME") .. "/.local/bin")
