077248871a32e9acd4f05b43f3dc454712cc64c9
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-08-28 17:22:10 +0700

    [FIX] Changed terminal in autostart to use terminal launch script (Fine no more of this comments... (starting from now obviously-))

--- home/.config/hypr/autostart.lua
+++ home/.config/hypr/autostart.lua
@@ -8,7 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 
  hl.on("hyprland.start", function () 
-   hl.exec_cmd(terminal)
+   hl.exec_cmd("vj-launch-terminal")
    hl.exec_cmd("nm-applet")
    hl.exec_cmd("waybar & hyprpaper")
  end)
