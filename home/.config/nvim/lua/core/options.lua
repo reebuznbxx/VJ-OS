d94bdc5fd8652db503c5c2839f5430b29e089dd9
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-08-29 04:27:56 +0700

    [FEAT] Added neovim config (Try copying with "y"-ank in visual mode!)

---
+++ home/.config/nvim/lua/core/options.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
