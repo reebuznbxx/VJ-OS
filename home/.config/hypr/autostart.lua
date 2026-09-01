8a648b783660c53bd9153df8fb0aa95ff3069045
reebuznbxx <reebuznb@gmail.com>
2026-09-01 05:22:53 +0500

    Hyprlockgit add home/.config/hypr/autostart.lua

--- home/.config/hypr/autostart.lua
+++ home/.config/hypr/autostart.lua
@@ -7,8 +7,11 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 
- hl.on("hyprland.start", function () 
-   hl.exec_cmd("vj-launch-terminal")
-   hl.exec_cmd("nm-applet")
-   hl.exec_cmd("waybar & hyprpaper")
+local james = hl    -- :D
+
+ james.on("hyprland.start", function () 
+   james.exec_cmd("vj-launch-terminal")
+   james.exec_cmd("nm-applet")
+   james.exec_cmd("waybar & hyprpaper")
+   james.exec_cmd("hyprlock")
  end)
