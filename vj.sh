093999b1ba8ee01523c01c49fb260799ddf6f9ea
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-09-03 03:08:50 +0700

    [FIX] Fix stow linking every files instead of just home/

--- vj.sh
+++ vj.sh
@@ -2,7 +2,7 @@
 
 # Find the parent directory of the script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[@]}" )" &>/dev/null && pwd )
-STOW_DIR="$SCRIPT_DIR"
+STOW_DIR="$SCRIPT_DIR/home"
 
 export STOW_DIR
 
